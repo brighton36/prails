936d92ec94683d1e5a5efc4368d0af614b32b720
brighton36 <chris@chrisderose.com>
2020-09-15 11:14:54 -0400

    updated the cli parameters to use a command format, instead of a switch format. Added an output command

--- lib/libprails.cpp
+++ lib/libprails.cpp
@@ -1,4 +1,7 @@
+#include <iostream>
 #include <filesystem>
+
+#include "httplib.h"
 
 #include "prails.hpp"
 #include "server.hpp"
@@ -9,49 +12,123 @@ using namespace std;
 using namespace Pistache;
 using namespace prails::utilities;
 
-enum class RunMode { WebServer, Migration };
+enum class RunMode { Help, WebServer, Migration, OutputUrl };
 
 INIT_MODEL_REGISTRY()
 INIT_CONTROLLER_REGISTRY()
 
 int prails::main(int argc, char *argv[]) {
   string config_path;
-  RunMode run_mode = RunMode::WebServer;
+  RunMode run_mode = RunMode::Help;
 
   string program_name = filesystem::path(string(argv[0])).filename();
   vector<string> args(argv + 1, argv + argc);
 
-  for(string a : args) {
-    if (a.at(0) != '-') config_path = a;
-    else if (a == "-M") run_mode = RunMode::Migration;
-    else if (a == "-S") run_mode = RunMode::WebServer;
+  // NOTE: We remove the entries in the args list as we recognize them. We
+  //       save anything we don't recognize, for use below.
+	for (auto it = args.begin(); it != args.end(); it++) {
+		string arg = *it;
+		args.erase(it--);
+
+		if (arg == "-f") {
+			// Advance the pointer, and store the next string as our config path.
+			it++;
+			if (it == args.end()) 
+				throw invalid_argument("-f specified, but missing param");
+
+			config_path = *it;
+			args.erase(it--);
+		}
+		else if ( (arg == "--help") || (arg == "-h") ) {
+			run_mode = RunMode::Help;
+			break;
+		}
+		else if (arg == "migrate") {
+			run_mode = RunMode::Migration;
+			break;
+		}
+		else if (arg == "server") {
+			run_mode = RunMode::WebServer;
+			break;
+		}
+		else if (arg == "output") {
+			run_mode = RunMode::OutputUrl;
+			break;
+		}
+		else {
+			throw invalid_argument(fmt::format("Unrecognized argument \"{}\"", arg));
+			break;
+		}
+	}
+
+  ConfigParser config;
+
+  if (config_path.empty()) 
+    run_mode = RunMode::Help;
+  else {
+    spdlog::info("Using config={}", config_path);
+
+    config = ConfigParser(config_path);
+    Model::Initialize(config);
+    Controller::Initialize(config);
   }
-
-  if ( (args.size() == 0 ) || has_any(args, {"-h", "--help"}) || (config_path.empty() ) ) {
-    cout << fmt::format("Usage: {} [-S] [-M] CONFIG_FILE\n"
-      "A web server built with prails.\n\n"
-      "-S           Run in server mode.\n"
-      "-M           Run model migrations.\n"
-      "--help       Display this help and exit.\n\n"
-      "The supplied CONFIG_FILE is expected to be a yaml-formatted server configuration file.\n"
-      "(See https://en.wikipedia.org/wiki/YAML for details on the YAML file format.)\n\n",
-      program_name);
-    return 1;
-  }
-
-  spdlog::info("Using config={}", config_path);
-
-  ConfigParser config(config_path);
-  Model::Initialize(config);
-  Controller::Initialize(config);
 
   switch (run_mode) {
+    case RunMode::Help: {
+      cout << fmt::format("Usage: {} [-f CONFIG_FILE] COMMAND\n"
+        "A web server built with prails.\n\n"
+        "Global Switches:\n" 
+        "  -f CONFIG_FILE Use the supplied file for configuration settings.\n"
+        "  --help         Display this help and exit.\n\n"
+        "The following commands can be executed:\n"
+        "  server         Run in server mode.\n"
+        "  migrate        Run model migrations.\n"
+        "  output URL [FILE]  Output a URL to stdout (default) or [FILE].\n"
+        "The supplied CONFIG_FILE is expected to be a yaml-formatted server configuration file.\n"
+        "(See https://en.wikipedia.org/wiki/YAML for details on the YAML file format.)\n\n",
+        program_name);
+      return 1;
+    }
     case RunMode::WebServer: {
       spdlog::info("{} log started. Cores={} Threads={}", program_name,
         hardware_concurrency(), config.threads());
       Server server(config);
       server.start();
       } break;
+    case RunMode::OutputUrl: {
+      string url; 
+      string output; 
+      
+      if (args.size() > 0) {
+        url = args[0];
+        if (args.size() > 1) output = args[1];
+      } else
+        throw invalid_argument("Missing output parameter(s)");
+
+      spdlog::info("Outputting {} to {}.", url, 
+        (output.empty()) ? "STDOUT" : output);
+
+      Server server(config);
+      server.startThreaded();
+
+      auto addr = Pistache::Address(config.address(), config.port());
+      auto browser = httplib::Client(addr.host().c_str(), (uint16_t) addr.port());
+      auto res = browser.Get(url.c_str());
+
+      if (res->status != 200)
+        throw logic_error(fmt::format("Error when requesting resource: {}", 
+          res->status));
+
+      if (output.empty())
+        cout << res->body << endl;
+      else {
+        std::ofstream out(output);
+        out << res->body;
+        out.close();
+      }
+
+      server.shutdown();
+    } break;
     case RunMode::Migration: {
       spdlog::info("{} migration.", program_name);
 
@@ -59,7 +136,7 @@ int prails::main(int argc, char *argv[]) {
         spdlog::info("Running migration for {}..", reg);
         ModelFactory::migrate(reg);
       }
-      } break; 
+    } break; 
   }
 
   return 0;
